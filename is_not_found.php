d826d7decbd884ee1e7470d4ce0e6d650aaf6ee1
okusan <okuson2012@gmail.com>
2021-07-13 21:46:08 +0900

    first commit

---
+++ is_not_found.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * ‚S‚O‚S‚©”Û‚©‚ð•Ô‚·B
+ * @param $url ‘ÎÛ‚ÌURL
+ * @return bool
+ */
+function is_not_found($url)
+{
+    $ch = curl_init($url);
+
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+
+    $info = curl_getinfo($ch);
+
+    $is_error = false;
+    if (404 == $info['http_code']) {
+        $is_error = true;
+    }
+
+    curl_close($ch);
+
+    if ($is_error) {
+        return true;
+    } else {
+        return false;
+    }
+}
